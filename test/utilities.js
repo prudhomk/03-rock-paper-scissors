9eaa7482b82d43224b5219d4fcd4e876fe46150a
Kalan Prudhomme <cleaver9@gmail.com>
2021-03-25 14:00:57 -0700

    sorting functions

--- test/utilities.js
+++ test/utilities.js
@@ -1,6 +1,29 @@
-export function checkHand(userHand, roshamboHand) {
-    if (userHand > roshamboHand) return 1;
-    if (userHand < roshamboHand) return -1;
-    if (userHand === roshamboHand) return 0;
-    
+export function checkHand(userHand, compHand) {
+    if (userHand === 1 && compHand === 2) return 1;
+    if (userHand === 2 && compHand === 3) return 2;
+    if (userHand === 3 && compHand === 1) return 3;
+    if (userHand === compHand) return 4;
+    if (userHand === 2 && compHand === 1) return 5;
+    if (userHand === 3 && compHand === 2) return 6;
+    if (userHand === 1 && compHand === 3) return 7;
+}
+
+
+
+
+export function roshambo() {
+    const randomNum = Number(Math.ceil(Math.random) * 3);
+    //Scissors
+    if (randomNum === 3) {
+        let compHand = 3;
+    }
+    //Paper
+    if (randomNum === 2) {
+        let compHand = 2;
+    }
+    //Rock
+    if (randomNum === 1) {
+        let compHand = 1;
+    }
+
 }
